a88b6ebc773be3a232c100b4ea7ca977159cc0f2
huntercd <hunter641@qq.com>
2018-02-06 11:19:26 +0800

    use vendor
    Use vendor for project,  and import config files.

--- server/src/gate/main.go
+++ server/src/gate/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"github.com/golang/glog"
+)
+
+var (
+	cfg     string
+	version bool
+)
+
+func init() {
+	flag.StringVar(&cfg, "config", "cfg.json", "config file")
+	flag.BoolVar(&version, "version", false, "show version")
+}
+
+var (
+	buildstamp string
+	githash    string
+)
+
+func showVersion() {
+	glog.Info("Git Commit Hash: %v\tBuild Time:%v\n", githash, buildstamp)
+}
+
 
 func main() {
-	fmt.Println("hello gate")
+	flag.Parse()
+
+	showVersion()
 }
---
+++ server/src/gate/service.sh
@@ -0,0 +1,89 @@
+exe=gate
+logLevel=2
+exePid=$exe.pid
+logfile=console.log
+
+mkdir -p log
+
+cwd=$(pwd)
+export CLIENT_PUBLIC_KEY=$cwd/key/client_public_key.pem
+export CLIENT_PRIVATE_KEY=$cwd/key/client_private_key.pem
+
+export SERVER_PUBLIC_KEY=$cwd/key/server_public.pem
+export SERVER_PRIVATE_KEY=$cwd/key/server_private.key
+
+export GAME_WEB_CERT_FILE=$cwd/key/gameweb.qyq.xoyo.pem
+export GAME_WEB_KEY_FILE=$cwd/key/gameweb.qyq.xoyo.key
+
+export ROBOT_KEY=4842e8023d991410547bc971ea39e656
+
+function check_pid() {
+	if [ -f $exePid ];then
+		pid=`cat $exePid`
+		if [ -n $pid ];then
+			running=`ps -p $pid|grep -v "PID TTY" | wc -l`
+			return $running
+		fi
+	fi
+	return 0
+}
+
+function start() {
+	check_pid
+	running=$?
+	if [ $running -gt 0 ];then
+		echo -n "$exe now is running already, pid="
+		cat $exePid
+		return 1
+	fi
+
+	nohup ./$exe -v=$logLevel -logtostderr=true -log_dir=./log &> $logfile &
+	echo $! > $exePid
+	echo "$exe started..., pid=$!"
+}
+
+function stop() {
+	pid=`cat $exePid`
+	kill $pid
+	echo "$exe stopped..."
+}
+
+function restart() {
+	stop
+	sleep 1
+	start
+}
+
+function status() {
+	check_pid
+	running=$?
+	if [ $running -gt 0 ];then
+		echo -n "$exe now is running, pid="
+		cat $exePid
+	else
+		echo "$exe is stopped"
+	fi
+}
+
+function tailf() {
+	tail -f $logfile
+}
+
+function help() {
+	echo "$0 start|stop|restart|status|tail"
+}
+
+if [ "$1" == "" ];then
+	help
+elif [ "$1" == "stop" ];then
+	stop
+elif [ "$1" == "start" ];then
+	start
+elif [ "$1" == "restart" ];then
+	restart
+elif [ "$1" == "status" ];then
+	status
+elif [ "$1" == "tail" ];then
+	tailf
+fi
+
