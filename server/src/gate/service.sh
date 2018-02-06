ef5e22ae6d022972c8072ee21988fb9b5bafe238
huntercd <hunter641@qq.com>
2018-02-06 17:09:53 +0800

    delete key

--- server/src/gate/service.sh
+++ server/src/gate/service.sh
@@ -6,16 +6,8 @@ logfile=console.log
 mkdir -p log
 
 cwd=$(pwd)
-export CLIENT_PUBLIC_KEY=$cwd/key/client_public_key.pem
-export CLIENT_PRIVATE_KEY=$cwd/key/client_private_key.pem
 
-export SERVER_PUBLIC_KEY=$cwd/key/server_public.pem
-export SERVER_PRIVATE_KEY=$cwd/key/server_private.key
-
-export GAME_WEB_CERT_FILE=$cwd/key/gameweb.abc.pem
-export GAME_WEB_KEY_FILE=$cwd/key/gameweb.abc.key
-
-export ROBOT_KEY=abcdefg123456
+export KEY=abcdefg123456
 
 function check_pid() {
 	if [ -f $exePid ];then
